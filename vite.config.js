314b26946af2b3257767ca3353b3d431b925673d
Frida Ljungberg <frlj2501@student.miun.se>
2026-03-11 16:04:42 +0100

    Lagt till image-optimizer

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html")
+            }
+        }
+    },
+    plugins: [
+        ViteImageOptimizer({
+            webp: {
+                quality: 75
+            },
+            jpg: {
+                quality: 75
+            }
+        })
+    ]
+});
